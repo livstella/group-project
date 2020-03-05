a122323ec4a51839a4c91455ef6af5fb6f19f705
NiklasReents <61356983+NiklasReents@users.noreply.github.com>
2020-03-05 19:47:55 +0100

    Update index.js
    added the correct links to the alert function

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function myFunction() {
- alert("Hello! This the the url you need: https://runningonrealfood.com/vegan-buffalo-cauliflower-wraps/");
-}
+button1.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/vegan-buffalo-cauliflower-wraps/");
+});
+button2.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/brown-rice-burrito-bowl/");
+});
+button3.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/vegan-sweet-potato-tofu-bowls/");
+});
+button4.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: http://www.eatingwell.com/recipe/268220/vegan-mushroom-stroganoff/");
+});
+button5.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: http://www.eatingwell.com/recipe/257348/vegan-pancakes/");
+});
+button6.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/chickpea-veggie-burgers/");
+});
