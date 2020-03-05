de9aa0f4fdc03d2c97fa58ea38cc1de618110846
NiklasReents <61356983+NiklasReents@users.noreply.github.com>
2020-03-05 19:59:19 +0100

    Update index.js
    added href functionality to the weblinks

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 button1.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: https://runningonrealfood.com/vegan-buffalo-cauliflower-wraps/");
+ alert("Share this link with your friends: <a href="https://runningonrealfood.com/vegan-buffalo-cauliflower-wraps/"></a>");
 });
 button2.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: https://runningonrealfood.com/brown-rice-burrito-bowl/");
+ alert("Share this link with your friends: <a href="https://runningonrealfood.com/brown-rice-burrito-bowl/"></a>");
 });
 button3.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: https://runningonrealfood.com/vegan-sweet-potato-tofu-bowls/");
+ alert("Share this link with your friends: <a href="https://runningonrealfood.com/vegan-sweet-potato-tofu-bowls/"></a>");
 });
 button4.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: http://www.eatingwell.com/recipe/268220/vegan-mushroom-stroganoff/");
+ alert("Share this link with your friends: <a href="http://www.eatingwell.com/recipe/268220/vegan-mushroom-stroganoff/"></a>");
 });
 button5.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: http://www.eatingwell.com/recipe/257348/vegan-pancakes/");
+ alert("Share this link with your friends: <a href="http://www.eatingwell.com/recipe/257348/vegan-pancakes/"></a>");
 });
 button6.addEventListener("click", function myFunction(){
- alert("Share this link with your friends: https://runningonrealfood.com/chickpea-veggie-burgers/");
+ alert("Share this link with your friends: <a href="https://runningonrealfood.com/chickpea-veggie-burgers/"></a>");
 });
